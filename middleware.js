d296e6cb3430474e3c854ab3ee0865a7096204f9
guanvictor <guanvictor@hotmail.com>
2019-03-27 09:53:41 -0700

    Initial Commit

---
+++ middleware.js
@@ -0,0 +1,62 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express();
+
+app.use(express.static(__dirname + '/public'));
+
+app.use((request, response, next) => {
+
+});
+
+app.get('/', (request, response) => {
+    // response.send('<h1>Hello Express!</h1>');
+    response.send({
+        name: 'Your name',
+        school: [
+            'BCIT',
+            'SFU',
+            'UBC'
+        ]
+    })
+});
+
+
+
+//
+// app.get('/info', (request,response) => {
+//     response.render('about.hbs', {
+//         title: 'UNF',
+//         year: new Date().getFullYear()
+//         //welcome: 'Hello!'
+//     });
+// });
+
+app.get('/404', (request,response) => {
+    response.send({
+        error: 'Page not found'
+    })
+});
+
+app.use((request, response, next) => {
+    var time = new Date().toString();
+    var log = `${time}: ${request.method} ${request.url}`;
+    fs.appendFile('server.log'), log + '\n', (error) => {
+        if (error) {
+            console.log('Unable to log message');
+        }
+    };
+    //console.log(`${time}: ${request.method} ${request.url}`);
+    next();
+});
+
+app.use((request, response, next) => {
+    response.render("maintenance.html")
+});
+
+
+
+app.listen(8080, () => {
+    console.log('Server is up on port 8080');
+});
